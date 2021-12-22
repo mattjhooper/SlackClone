7ffc2cd00aac615e3c113b6bbe1a7e703d9a03c9
mattjhooper <matt.j.hooper@googlemail.com>
2021-12-22 16:37:23 +0000

    Basic messages

---
+++ SlackClone/SlackClone/Domain/SlackMessage.cs
@@ -0,0 +1,9 @@
+﻿namespace SlackClone.Domain
+{
+    public class SlackMessage
+    {
+        public string PostedBy { get; set; }
+        public DateTime PostedOn { get; set; }
+        public string Message { get; set; }
+    }
+}
