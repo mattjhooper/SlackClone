bde38faf9ca1f764b1e4dfc9033cc9b31bc6dd41
mattjhooper <matt.j.hooper@googlemail.com>
2021-12-22 18:09:19 +0000

    Added reactions to message

--- SlackClone/SlackClone/Domain/SlackMessage.cs
+++ SlackClone/SlackClone/Domain/SlackMessage.cs
@@ -2,8 +2,26 @@
 {
     public class SlackMessage
     {
+        private string[] icons = {"oi oi-thumb-up","oi oi-thumb-down", "oi oi-rain", "oi oi-sun" };
+
+
+        private readonly Dictionary<string, int> _reactions;
+
+        public SlackMessage()
+        {
+            _reactions = new Dictionary<string,int>();
+
+            foreach(var icon in icons)
+            {
+                _reactions[icon] = 0;
+            }
+        }
+        
         public string PostedBy { get; set; }
         public DateTime PostedOn { get; set; }
         public string Message { get; set; }
+
+        public Dictionary<string, int> Reactions => _reactions;
     }
 }
+
